1769338e5097f57a311d64300146cae8455e5dcf
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-08-02 19:49:26 -0400

    Fixed JS/JQ loading problem in homepage/contact

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-var fiveKList = document.getElementById('fivek'); //selects #fivek on homepage
-var finalCountdown = document.createElement('div'); //creates a div
-finalCountdown.innerHTML = "<h3 id='jsh3'>It's the Final Countdown</h3>"; //adds h3 to the div
-fiveKList.appendChild(finalCountdown);
-
 $(document).ready(function () {
 
     $('#rateYo').rateYo({
         rating: 3.6
     });
 
-    Placeholdem( document.querySelectorAll('[placeholder]') );
+    Placeholdem(document.querySelectorAll('[placeholder]') );
     var fadeElems = document.body.querySelectorAll('.fade'),
         fadeElemsLength = fadeElems.length,
         i = 0,
@@ -24,3 +19,9 @@ $(document).ready(function () {
     }
     setTimeout(incFade, interval);
 });
+
+
+var fiveKList = document.getElementById('fivek'); //selects #fivek on homepage
+var finalCountdown = document.createElement('div'); //creates a div
+finalCountdown.innerHTML = "<h3 id='jsh3'>It's the Final Countdown</h3>"; //adds h3 to the div
+fiveKList.appendChild(finalCountdown);
