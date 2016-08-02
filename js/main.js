9c2dcc91489355f7330b1bc82841edc8f3ba0216
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-08-02 16:30:58 -0400

    Add a jQuery plugin to the form's placeholders

--- js/main.js
+++ js/main.js
@@ -4,4 +4,18 @@ $(document).ready(function () {
         rating: 3.6
     });
 
+    Placeholdem( document.querySelectorAll('[placeholder]') );
+    var fadeElems = document.body.querySelectorAll('.fade'),
+        fadeElemsLength = fadeElems.length,
+        i = 0,
+        interval = 50;
+
+    function incFade() {
+        if (i < fadeElemsLength) {
+            fadeElems[ i ].className += ' fade-load';
+            i++;
+            setTimeout(incFade, interval);
+        }
+    }
+    setTimeout(incFade, interval);
 });
